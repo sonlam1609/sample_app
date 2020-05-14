da143b1a2f9542488cbc71d10faf8cd33f20bec5
sonlam1609 <nguyenlamson1609@gmail.com>
2020-05-14 14:11:41 +0700

    add chap07

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,31 @@
 class UsersController < ApplicationController
-  def new; end
+  def show
+    @user = User.find_by id: params[:id]
+    return if @user
+      flash[:danger] = t "static_page.user.undefine"
+      redirect_to root_path
+    
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+
+    if @user.save
+      flash[:danger] = t "static_page.user.welcome"
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  private
+  
+  def user_params
+  	params.require(:user).permit User::USER_PARAMS
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2020_05_07_081333) do
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
