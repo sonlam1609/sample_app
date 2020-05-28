d3b7e342466d699eb4621c1777764d295d8ea001
sonlam1609 <nguyenlamson1609@gmail.com>
2020-05-28 09:51:40 +0700

    add chap11

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
 
   VALID_EMAIL_REGEX = Settings.reg.email
   USER_PARAMS = [:name, :email, :password, :password_confirmation].freeze
-  before_save :email_down
+  before_save :downcase_email
+  before_create :create_activation_digest
+
   validates :name, presence: true, length: { maximum: Settings.reg.maxname }
   validates :email, format: { with: VALID_EMAIL_REGEX },
                              presence: true, uniqueness: true, length: { maximum: Settings.reg.maxemail }
@@ -27,17 +29,32 @@ class User < ApplicationRecord
     update_attribute :remember_digest, User.digest(remember_token)
   end
 
-  def authenticated? remember_token
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
-  end
-
   def forget
     update_attribute :remember_digest, nil
   end
 
+  def authenticated? attribute, token
+    digest = send "#{attribute}_digest"
+    return false unless digest
+    BCrypt::Password.new(digest).is_password? token
+  end
+  
+  def activate
+    update_attributes activated: true, activated_at: Time.zone.now
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
   private
 
-  def email_down 
+  def downcase_email
     email.downcase!
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
