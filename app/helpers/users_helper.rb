4c8e3f9a8d5198d8bdd588ab170f5085c206870d
sonlam1609 <nguyenlamson1609@gmail.com>
2020-06-04 16:06:09 +0700

    add chap14

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,11 @@
 module UsersHelper
   def gravatar_for user, size: Settings.avata_size
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_id = Digest::MD5::hexdigest(current_user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag gravatar_url, alt: user.name, class: "gravatar"
+    image_tag gravatar_url, alt: current_user.name, class: "gravatar"
+  end
+
+  def unfollow
+    current_user.active_relationships.find_by followed_id: @user.id
   end
 end
